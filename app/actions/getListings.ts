c8d6a205236b53897ff878094bc493136ac20122
Devic <devic2202@gmail.com>
2023-05-04 20:06:15 +0700

    update code reverse

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -26,33 +26,56 @@ export default async function getListings(
       category,
     } = params;
 
-    const query: any = {
-      ...(userId && { userId }),
-      ...(category && { category }),
-      ...(roomCount && { roomCount: { gte: +roomCount } }),
-      ...(bathroomCount && { bathroomCount: { gte: +bathroomCount } }),
-      ...(guestCount && { guestCount: { gte: +guestCount } }),
-      ...(locationValue && { locationValue }),
-      ...(startDate &&
-        endDate && {
-          NOT: {
-            reservations: {
-              some: {
-                OR: [
-                  {
-                    endDate: { gte: startDate },
-                    startDate: { lte: startDate },
-                  },
-                  {
-                    startDate: { lte: endDate },
-                    endDate: { gte: endDate },
-                  },
-                ],
+    let query: any = {};
+
+    if (userId) {
+      query.userId = userId;
+    }
+
+    if (category) {
+      query.category = category;
+    }
+
+    if (roomCount) {
+      query.roomCount = {
+        gte: +roomCount
+      }
+    }
+
+    if (guestCount) {
+      query.guestCount = {
+        gte: +guestCount
+      }
+    }
+
+    if (bathroomCount) {
+      query.bathroomCount = {
+        gte: +bathroomCount
+      }
+    }
+
+    if (locationValue) {
+      query.locationValue = locationValue;
+    }
+
+    if (startDate && endDate) {
+      query.NOT = {
+        reservations: {
+          some: {
+            OR: [
+              {
+                endDate: { gte: startDate },
+                startDate: { lte: startDate }
               },
-            },
-          },
-        }),
-    };
+              {
+                startDate: { lte: endDate },
+                endDate: { gte: endDate }
+              }
+            ]
+          }
+        }
+      }
+    }
 
     const listings = await prisma.listing.findMany({
       where: query,
